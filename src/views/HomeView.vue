ac255ba8f642aa2368209000ef8009ea7b8c7167
sanng97 <sanng97@naver.com>
2022-09-15 15:28:34 +0900

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,136 @@
+<template>
+<div>
+<swiper :options="swiperOption" class="swiper">
+  <swiper-slide  class="slide" style="background-image:url(/image/dog1.jpg);">
+    <v-layout data-aos="fade-up" fill-height align-center justify-center>
+      <div class="slog">명지전문대학 박상언</div>
+    </v-layout>
+
+  </swiper-slide>
+  <swiper-slide class="slide" style="background-image:url(/image/dog2.jpg);">
+    <v-layout fill-height align-center justify-center>
+      <div class="slog">열심히하겠습니다</div>
+     </v-layout>
+  </swiper-slide>
+  <swiper-slide class="slide" style="background-image:url(/image/dog3.jpg);">
+    <v-layout fill-height align-center justify-center>
+      <div class="slog">뽑아주세요</div>
+     </v-layout>
+  </swiper-slide>
+</swiper>
+
+<div class="section">
+<div class="header">프로젝트 목록</div>
+<v-layout wrap>
+
+  <v-flex xs4 class="pa-2" data-aos="fade-right">
+    <v-responsive :aspect-ratio="1/1">
+    <img src="/image/dog1.jpg" class="image">
+  </v-responsive>
+  </v-flex>
+  <v-flex xs4 class="pa-2" data-aos="fade-right" data-aos-delay="100">
+    <v-responsive :aspect-ratio="1/1">
+    <img src="/image/dog2.jpg" class="image">
+  </v-responsive>
+  </v-flex>
+  <v-flex xs4 class="pa-2" data-aos="fade-down" data-aos-delay="200">
+    <v-responsive :aspect-ratio="1/1">
+    <img src="/image/dog3.jpg" class="image">
+  </v-responsive>
+  </v-flex>
+  <v-flex xs4 class="pa-2" data-aos="fade-left">
+    <v-responsive :aspect-ratio="1/1">
+    <img src="/image/dog1.jpg" class="image">
+  </v-responsive>
+  </v-flex>
+  <v-flex xs4 class="pa-2" data-aos="fade-right">
+    <v-responsive :aspect-ratio="1/1">
+    <img src="/image/dog2.jpg" class="image">
+  </v-responsive>
+  </v-flex>
+  <v-flex xs4 class="pa-2" data-aos="fade-left">
+    <v-responsive :aspect-ratio="1/1">
+    <img src="/image/dog3.jpg" class="image">
+  </v-responsive>
+  </v-flex>
+  
+
+
+
+
+</v-layout>
+</div>
+
+</div>
+</template>
+
+<script>
+ import { Swiper, SwiperSlide} from "vue-awesome-swiper" 
+  import "swiper/swiper-bundle.css"
+import AOS from "aos"
+import "aos/dist/aos.css";
+
+  export default {
+    name: 'Home',
+    data(){
+      return {
+      swiperOption:{
+        loop:true,
+        autoplay:{
+          delay:1000
+        }
+        }
+      }
+    },
+
+  components:{
+      Swiper,
+      SwiperSlide
+  },
+  mounted(){
+    AOS.init()
+  }
+
+  }
+</script>
+
+<style scoped lang="less">
+  .swiper{
+    height:100vh;
+    .slide{
+      background-size:cover;
+      background-position: center center;
+      .slog{
+        font-size:60px;
+        font-weight:bold;
+        color:white;
+      }
+    }
+  }
+
+.section{
+
+  width:1190px;
+  margin: 0 auto;
+  padding-top:40px;
+  padding-top:40px;
+  .header{
+    text-align:center;
+    font-size:40px;
+    font-weight:bold;
+  }
+  .image{
+    width:100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+
+
+
+
+
+}
+
+
+</style>
